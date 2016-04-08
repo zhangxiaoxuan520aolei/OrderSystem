3caba8533a36b56bced83748ab6466e2bb9e7893
aolei <807648567@qq.com>
2016-04-08 19:58:32 +0800

    添加了textview_border.xml和startActivity,textview_border.xml是textView的边框文件，startActivity是启动activity，用户可以选择注册和登录

--- app/src/main/java/com/aolei/jxustnc/ordersystem/Activity/MainActivity.java
+++ app/src/main/java/com/aolei/jxustnc/ordersystem/Activity/MainActivity.java
@@ -1,36 +1,29 @@
-package com.aolei.jxustnc.ordersystem;
+package com.aolei.jxustnc.ordersystem.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity
+import com.aolei.jxustnc.ordersystem.R;
+
+public class MainActivity extends Activity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
