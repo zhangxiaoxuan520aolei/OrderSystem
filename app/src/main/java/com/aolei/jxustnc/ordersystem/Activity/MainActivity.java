d1bb0d991160dff50bd94be329a9129da8d23736
aolei <807648567@qq.com>
2016-04-28 01:34:29 +0800

    写好了自动登录 ，还有购买历史记录

--- app/src/main/java/com/aolei/jxustnc/ordersystem/Activity/MainActivity.java
+++ app/src/main/java/com/aolei/jxustnc/ordersystem/Activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.aolei.jxustnc.ordersystem.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -10,26 +13,43 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aolei.jxustnc.ordersystem.R;
 import com.aolei.jxustnc.ordersystem.fragment.CanteenFragment1;
 import com.aolei.jxustnc.ordersystem.fragment.CanteenFragment2;
 import com.aolei.jxustnc.ordersystem.fragment.CanteenFragment3;
 import com.aolei.jxustnc.ordersystem.fragment.HomeFragment;
+import com.aolei.jxustnc.ordersystem.fragment.PurchaseFragment;
 
-public class MainActivity extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     private Toolbar toolbar;
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
     private Fragment currentFragment;
     private NavigationView navigationView;
+    private LinearLayout layout_login_regist;
+    private SharedPreferences mSharedPreferences;
+    private TextView mStatusText;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences("user_info", Context.MODE_APPEND);
+        mNavigationView = (NavigationView)findViewById(R.id.nav_view);
+        View view  = mNavigationView.getHeaderView(0);//获取NavigationView的heardLayout
+        mStatusText = (TextView)view.findViewById(R.id.status);//通过heardLayout来获取TextView 控件
+        if (mSharedPreferences.getBoolean("status",false) == true){
+            mStatusText.setText(mSharedPreferences.getString("username",""));
+    }
         initView();
         initEvent();
     }
@@ -42,12 +62,16 @@ public class MainActivity extends FragmentActivity implements NavigationView.OnN
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
+        layout_login_regist.setOnClickListener(this);
     }
 
     /**
      * 初始化控件
      */
     private void initView() {
+        View v = LayoutInflater.from(this).inflate(R.layout.nav_header_main, null);
+        layout_login_regist = (LinearLayout) v.findViewById(R.id.layout_login_regist);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setTitle("推荐");
@@ -59,6 +83,7 @@ public class MainActivity extends FragmentActivity implements NavigationView.OnN
         HomeFragment homeFragment = new HomeFragment();
         currentFragment = homeFragment;
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, homeFragment).commit();
+
     }
 
     /**
@@ -120,8 +145,18 @@ public class MainActivity extends FragmentActivity implements NavigationView.OnN
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_purchase) {
+            toolbar.setTitle("我的购买");
+            switchFragment(new PurchaseFragment());
+        }else if (id == R.id.signout){
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putString("username","");
+            editor.putString("userpwd","");
+            editor.commit();
+            Intent intent = new Intent();
+            intent.setClass(this,LoginActivity.class);
+            startActivity(intent);
+            finish();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -139,4 +174,15 @@ public class MainActivity extends FragmentActivity implements NavigationView.OnN
         }
         currentFragment = fragment;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layout_login_regist:
+                Toast.makeText(this, "点击", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/aolei/jxustnc/ordersystem/Activity/ShowFoodActivity.java
+++ app/src/main/java/com/aolei/jxustnc/ordersystem/Activity/ShowFoodActivity.java
@@ -2,32 +2,124 @@ package com.aolei.jxustnc.ordersystem.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.ListView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.aolei.jxustnc.ordersystem.R;
+import com.aolei.jxustnc.ordersystem.adapter.RecyclerViewCommonAdapter;
+import com.aolei.jxustnc.ordersystem.adapter.RecyclerViewViewHolder;
+import com.aolei.jxustnc.ordersystem.entity.Food;
 import com.aolei.jxustnc.ordersystem.entity.Store;
-import com.aolei.jxustnc.ordersystem.util.Utils;
+import com.aolei.jxustnc.ordersystem.util.CheckUtils;
+import com.aolei.jxustnc.ordersystem.util.HttpUtils;
+import com.aolei.jxustnc.ordersystem.util.NetworkUtil;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
 
-public class ShowFoodActivity extends AppCompatActivity {
-    private ListView mListView;
+public class ShowFoodActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private Toolbar mToolbar;
     private Store store;
+    private RecyclerView mRecyclerView;
+    private List<Food> mFoodList;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerViewCommonAdapter<Food> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Bmob.initialize(this, Utils.APPID);
+        Bmob.initialize(this, CheckUtils.APPID);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_food);
         initView();
         Intent intent = this.getIntent();
-        store = (Store)intent.getSerializableExtra("store_info");
+        store = (Store) intent.getSerializableExtra("store_info");
         mToolbar.setTitle(store.getStore_name());
+        getNetDate();
+
+
     }
-    void initView(){
-        mListView = (ListView)findViewById(R.id.store_food);
-        mToolbar = (Toolbar)findViewById(R.id.store_title);
+
+    void initView() {
+        mToolbar = (Toolbar) findViewById(R.id.store_title);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.ShowFood);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.showfood_recyclerview);
     }
+
+    @Override
+    public void onRefresh() {
+        getNetDate();
+    }
+
+    /**
+     * 获取网络数据
+     */
+    private void getNetDate() {
+        if (NetworkUtil.isConnected(getApplicationContext())) {
+            BmobQuery<Food> query = new BmobQuery<>();
+            HttpUtils.queryFoodByStoreName(getApplicationContext(), query, new FindListener<Food>() {
+                @Override
+                public void onSuccess(List<Food> list) {
+                    mFoodList = list;
+                    setRecyclerViewDate();
+                }
+
+                @Override
+                public void onError(int i, String s) {
+                    Log.d("error", s);
+                }
+            }, store.getObjectId());
+        } else {
+            Snackbar.make(mRecyclerView, "网络连接出错", Snackbar.LENGTH_SHORT).show();
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void setRecyclerViewDate() {
+        if (!mFoodList.isEmpty()) {
+            LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+            mRecyclerView.setLayoutManager(mLinearLayoutManager);
+            mAdapter = new RecyclerViewCommonAdapter<Food>(this, R.layout.item_home_recyclerview, mFoodList) {
+                @Override
+                public void convert(RecyclerViewViewHolder holder, Food food) {
+                    holder.setTextView(R.id.tv_shop_item_home, food.getStore().getStore_name());
+                    holder.setTextView(R.id.tv_canteen_item_home, food.getStore().getBelong_cateen());
+                    holder.setTextView(R.id.tv_name_item_home, food.getName());
+                    holder.setTextView(R.id.tv_price_item_home, "￥" + food.getPrice());
+                    holder.setTextView(R.id.tv_count_item_home, "已售" + food.getSold_count() + "份");
+                    ImageView img_item_home = holder.getView(R.id.img_item_home);
+                    Glide.with(getApplicationContext()).load(food.getImg_url()).error(R.drawable.imgloadfiald).into(img_item_home);//设置图片
+                }
+            };
+            mRecyclerView.setAdapter(mAdapter);
+            /**
+             * 每个Item 的点击事件，跳转到FoodDetaili界面
+             */
+            mAdapter.setmOnItemClickListener(new RecyclerViewCommonAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position) {
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("list", mFoodList.get(position));
+                    Intent intent = new Intent(getApplicationContext(), FoodDetailActivity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+            });
+        } else {
+            Log.d("error", "没有查询到数据");
+        }
+    }
+
 }
